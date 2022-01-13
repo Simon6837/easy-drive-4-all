436445f1c729da46bf3f1b2097f5e19b137e1f18
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2022-01-13 11:27:27 +0100

    working on the layout

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,17 +1,5 @@
 <section class="bg-white">
     <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
-        <nav class="flex flex-wrap justify-center -mx-5 -my-2">
-            <div class="px-5 py-2">
-                <a href="/autos" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-                    Voertuigen
-                </a>
-            </div>
-            <div class="px-5 py-2">
-                <a href="/contact" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-                    Contact
-                </a>
-            </div>
-        </nav>
         <div class="flex justify-center mt-8 space-x-6">
             <a href="" class="text-gray-400 hover:text-gray-500">
                 <span class="sr-only">Facebook</span>
@@ -34,7 +22,7 @@
 
         </div>
         <p class="mt-8 text-base leading-6 text-center text-gray-400">
-            © 2021 easydrive4all. All rights reserved.
+            © 2021 EasyDrive4All. All rights reserved.
         </p>
     </div>
 </section>
