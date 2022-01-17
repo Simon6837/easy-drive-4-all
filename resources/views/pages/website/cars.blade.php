f15eebbcc5fe17fa563ebe4f8f731c019c956743
DatReki <TimoHalofan@outlook.com>
2022-01-17 09:58:28 +0100

    (WIP) Made car page with placeholder data.

--- resources/views/pages/website/cars.blade.php
+++ resources/views/pages/website/cars.blade.php
@@ -1,3 +1,36 @@
 <x-app-layout>
-    <h1>Hallo Auto's</h1>
+    <div class="w-full bg-gray-100">
+        <div class="container max-w-5xl mx-auto mb-6 mt-8">
+            <div class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-4">
+                <!-- start item -->
+                <div class="mb-8 bg-white shadow rounded-lg">
+                    <img class="rounded-t-lg" src="assets/placeholder.png" alt="test" >
+                    <div class="px-4 py-8">
+                        <h2 class="text-lg font-semibold">Car name</h2>
+                        <ul class="list-none">
+                            <li>
+                                <div class="grid grid-cols-2">
+                                    <p>Merk:</p>
+                                    <p class="text-right">test 2</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="grid grid-cols-2">
+                                    <p>Type:</p>
+                                    <p class="text-right">test 2</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="grid grid-cols-2">
+                                    <p>Kenteken:</p>
+                                    <p class="text-right">test 2</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- end item -->
+            </div>
+        </div>
+    </div>
 </x-app-layout>
