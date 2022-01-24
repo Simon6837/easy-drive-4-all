10b3881e9d2aa8e9ec1ef07b246f4c01ad21ee5a
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2022-01-24 13:33:34 +0100

    Added devider and removed edit in the overview

--- resources/views/pages/instructors/absence/active.blade.php
+++ resources/views/pages/instructors/absence/active.blade.php
@@ -18,7 +18,7 @@
                     </div>
                 </div>
             </div>
-            <h1>Actief</h1>
+            <h1 class="font-bold text-black p-1">Actief</h1>
             <table class="border-collapse w-full">
                 <thead class="bg-gray-50">
                     <tr>
@@ -127,7 +127,7 @@
                 @endforeach
                 </tbody>
             </table>
-            <h1>Alle ziekmeldingen</h1>
+            <h1 class="font-bold text-black p-1">Alle ziekmeldingen</h1>
             <table class="border-collapse w-full">
                 <thead class="bg-gray-50">
                 <tr>
@@ -149,13 +149,6 @@
                         <div class="flex">
                             <div class="basis-4/6 md:basis-5/6 px-3 py-2 flex justify-center">
                                 <p>Einddatum</p>
-                            </div>
-                        </div>
-                    </th>
-                    <th class="p-3 font-bold uppercase bg-gray-200 text-gray-500 hidden lg:table-cell">
-                        <div class="flex">
-                            <div class="basis-4/6 md:basis-5/6 px-3 py-2 flex justify-center">
-                                <p>Bewerken</p>
                             </div>
                         </div>
                     </th>
@@ -207,27 +200,6 @@
                                         @else
                                             {{$absence->end_date}}
                                         @endif
-                                    </div>
-                                </div>
-                            </div>
-                        </td>
-                        <td class="w-full lg:w-auto text-center p-0 border-t">
-                            <div class="flex flex-row h-full">
-                                <div
-                                    class="basis-2/6 md:basis-1/6 lg:hidden bg-gray-200 text-gray-500 text-xs font-bold uppercase flex justify-center items-center px-3 py-2">
-                                    <span>Bewerken</span>
-                                </div>
-                                <div class="basis-4/6 md:basis-5/6 px-3 py-2">
-                                    <div class="flex justify-center lg:w-24">
-                                        <a class="cursor-pointer" href="edit/{{$absence->id}}">
-                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                 class="w-12 h-12 lg:w-8 lg:h-8 text-blue-400"
-                                                 fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                      stroke-width="2"
-                                                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
-                                            </svg>
-                                        </a>
                                     </div>
                                 </div>
                             </div>
